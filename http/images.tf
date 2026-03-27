d365a405fff75e22774ae524a3cb0a0989fd46e3
sp26-ojt <draculacoolboy@gmail.com>
2026-03-27 22:32:22 +0700

    Update images.tf

--- http/images.tf
+++ http/images.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "noble" {
   name             = "ubuntu-noble-x86_64"
-  image_source_url = "http://42.115.38.85:8080/noble-server-cloudimg-amd64.img"
+  image_source_url = "http://127.0.0.1:8080/noble-server-cloudimg-amd64.img"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -15,7 +15,7 @@ resource "openstack_images_image_v2" "noble" {
 
 resource "openstack_images_image_v2" "debian_12" {
   name             = "debian-12-x86_64"
-  image_source_url = "http://42.115.38.85:8080/debian-12-genericcloud-amd64.qcow2"
+  image_source_url = "http://127.0.0.1:8080/debian-12-genericcloud-amd64.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -31,7 +31,7 @@ resource "openstack_images_image_v2" "debian_12" {
 resource "openstack_images_image_v2" "kali" {
   count            = var.kali ? 1 : 0
   name             = "kali"
-  image_source_url = "http://42.115.38.85:8080/kali.qcow2"
+  image_source_url = "http://127.0.0.1:8080/kali.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -49,7 +49,7 @@ resource "openstack_images_image_v2" "kali" {
 resource "openstack_images_image_v2" "noble_man" {
   count            = var.noble_man ? 1 : 0
   name             = "ubuntu-noble-man"
-  image_source_url = "http://42.115.38.85:8080/ubuntu-noble-man.qcow2"
+  image_source_url = "http://127.0.0.1:8080/ubuntu-noble-man.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 

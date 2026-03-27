49b15932ca7c9473759732821c03fac5c94a1b3c
sp26-ojt <draculacoolboy@gmail.com>
2026-03-27 23:19:52 +0700

    local path

--- http/images.tf
+++ http/images.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "noble" {
   name            = "ubuntu-noble-x86_64"
-  local_file_path = "/home/ubuntu/images/noble-server-cloudimg-amd64.img"
+  local_file_path = "/vagrant/http/noble-server-cloudimg-amd64.img"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -15,7 +15,7 @@ resource "openstack_images_image_v2" "noble" {
 
 resource "openstack_images_image_v2" "debian_12" {
   name            = "debian-12-x86_64"
-  local_file_path = "/home/ubuntu/images/debian-12-genericcloud-amd64.qcow2"
+  local_file_path = "/vagrant/http/debian-12-genericcloud-amd64.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -31,7 +31,7 @@ resource "openstack_images_image_v2" "debian_12" {
 resource "openstack_images_image_v2" "kali" {
   count           = var.kali ? 1 : 0
   name            = "kali"
-  local_file_path = "/home/ubuntu/images/kali.qcow2"
+  local_file_path = "/vagrant/http/kali.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 
@@ -49,7 +49,7 @@ resource "openstack_images_image_v2" "kali" {
 resource "openstack_images_image_v2" "noble_man" {
   count           = var.noble_man ? 1 : 0
   name            = "ubuntu-noble-man"
-  local_file_path = "/home/ubuntu/images/ubuntu-noble-man.qcow2"
+  local_file_path = "/vagrant/http/ubuntu-noble-man.qcow2"
   container_format = "bare"
   disk_format      = "qcow2"
 

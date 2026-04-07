5c1fca87b6099f204a8d48c0390d414e045c0f69
sp26-ojt <draculacoolboy@gmail.com>
2026-04-07 11:16:14 +0700

    update patch file

--- scripts/patch-adaptive-training.py
+++ scripts/patch-adaptive-training.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+k#!/usr/bin/env python3
 import json, sys, os
 
 public_host = os.environ.get("PUBLIC_HOST", "")
@@ -8,16 +8,26 @@ data = cm.get("data", {})
 for key in data:
     if "elasticsearch.port=9200" in data[key]:
         lines = data[key].splitlines()
+        keys_to_remove = [
+            "server.base-url",
+            "frontend.url",
+            "spring.flyway.repair-on-migrate",
+            "spring.main.allow-bean-definition-overriding",
+        ]
+        lines = [l for l in lines if not any(l.startswith(k + "=") for k in keys_to_remove)]
+
         insert_at = next(i + 1 for i, l in enumerate(lines) if "elasticsearch.port=9200" in l)
         extra = [
             f"server.base-url=https://{public_host}/adaptive-training/api/v1",
             f"frontend.url=https://{public_host}",
             "spring.flyway.repair-on-migrate=true",
+            "spring.main.allow-bean-definition-overriding=true",
         ]
         for j, line in enumerate(extra):
-            if line.split("=")[0] not in data[key]:
-                lines.insert(insert_at + j, line)
+            lines.insert(insert_at + j, line)
+
         data[key] = "\n".join(lines)
 
 cm["data"] = data
 print(json.dumps(cm))
+
